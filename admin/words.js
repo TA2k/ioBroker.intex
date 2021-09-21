5f5431f1313e7835828bf614246b21f6561c3432
TA2k <tombox2020@gmail.com>
2021-09-21 19:44:13 +0200

    initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+    "intex adapter settings": {
+        en: "Adapter settings for intex",
+        de: "Adaptereinstellungen für intex",
+        ru: "Настройки адаптера для intex",
+        pt: "Configurações do adaptador para intex",
+        nl: "Adapterinstellingen voor intex",
+        fr: "Paramètres d'adaptateur pour intex",
+        it: "Impostazioni dell'adattatore per intex",
+        es: "Ajustes del adaptador para intex",
+        pl: "Ustawienia adaptera dla intex",
+        "zh-cn": "intex的适配器设置",
+    },
+    "App Email": {
+        en: "App Email",
+        de: "App-E-Mail",
+        ru: "Электронная почта приложения",
+        pt: "Email do aplicativo",
+        nl: "App-e-mail",
+        fr: "Courriel de l'application",
+        it: "E-mail dell'app",
+        es: "Correo electrónico de la aplicación",
+        pl: "E-mail aplikacji",
+        "zh-cn": "应用电子邮件",
+    },
+    "App Password": {
+        en: "App Password",
+        de: "App-Passwort",
+        ru: "Пароль приложения",
+        pt: "Senha de app",
+        nl: "App-wachtwoord",
+        fr: "Mot de passe de l'application",
+        it: "Password dell'app",
+        es: "Contraseña de la aplicación",
+        pl: "Hasło do aplikacji",
+        "zh-cn": "应用密码",
+    },
+};
---
+++ main.js
@@ -0,0 +1,333 @@
+"use strict";
+
+/*
+ * Created with @iobroker/create-adapter v2.0.1
+ */
+
+// The adapter-core module gives you access to the core ioBroker functions
+// you need to create an adapter
+const utils = require("@iobroker/adapter-core");
+const axios = require("axios");
+const Json2iob = require("./lib/json2iob");
+
+// Load your modules here, e.g.:
+// const fs = require("fs");
+
+class Intex extends utils.Adapter {
+    /**
+     * @param {Partial<utils.AdapterOptions>} [options={}]
+     */
+    constructor(options) {
+        super({
+            ...options,
+            name: "intex",
+        });
+        this.on("ready", this.onReady.bind(this));
+        this.on("stateChange", this.onStateChange.bind(this));
+        this.on("unload", this.onUnload.bind(this));
+    }
+
+    /**
+     * Is called when databases are connected and adapter received configuration.
+     */
+    async onReady() {
+        // Reset the connection indicator during startup
+        this.setState("info.connection", false, true);
+        if (this.config.interval < 0.5) {
+            this.log.info("Set interval to minimum 0.5");
+            this.config.interval = 0.5;
+        }
+        this.requestClient = axios.create();
+        this.updateInterval = null;
+        this.reLoginTimeout = null;
+        this.refreshTokenTimeout = null;
+        this.json2iob = new Json2iob(this);
+        this.deviceArray = [];
+        this.session = {};
+        this.commandObject = {};
+        this.objectEndings = {
+            "8888060F014000": "98",
+            "8888060FEE0F01": "DA",
+            "8888060F010400": "D4",
+            "8888060F010004": "D4",
+            "8888060F011000": "C8",
+            "8888060F010010": "C8",
+        };
+        this.subscribeStates("*");
+
+        await this.login();
+
+        if (this.session.token) {
+            await this.getDeviceList();
+            await this.updateDevices();
+            this.updateInterval = setInterval(async () => {
+                await this.updateDevices();
+            }, this.config.interval * 60 * 1000);
+            this.refreshTokenInterval = setInterval(() => {
+                this.refreshToken();
+            }, 1 * 60 * 60 * 1000); //1hour
+        }
+    }
+    async login() {
+        await this.requestClient({
+            method: "post",
+            url: "https://intexiotappservice.azurewebsites.net/api/oauth/auth",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "*/*",
+                "User-Agent": "Intex/1.0.13 (iPhone; iOS 14.8; Scale/3.00)",
+                "Accept-Language": "de-DE;q=1, en-DE;q=0.9",
+            },
+            data: JSON.stringify({
+                account: this.config.username,
+                password: new Buffer(this.config.password).toString("base64"),
+            }),
+        })
+            .then((res) => {
+                this.log.debug(JSON.stringify(res.data));
+                this.session = res.data;
+            })
+            .catch((error) => {
+                this.log.error(error);
+                if (error.response) {
+                    this.log.error(JSON.stringify(error.response.data));
+                }
+            });
+    }
+    async getDeviceList() {
+        await this.requestClient({
+            method: "get",
+            url: "https://intexiotappservice.azurewebsites.net/api/v1/userdevice/user",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "*/*",
+                "User-Agent": "Intex/1.0.13 (iPhone; iOS 14.8; Scale/3.00)",
+                "Accept-Language": "de-DE;q=1, en-DE;q=0.9",
+                Authorization: "Bearer " + this.session.token,
+            },
+        })
+            .then(async (res) => {
+                this.log.debug(JSON.stringify(res.data));
+                for (const device of res.data) {
+                    this.deviceArray.push(device.deviceId);
+                    await this.setObjectNotExistsAsync(device.deviceId, {
+                        type: "device",
+                        common: {
+                            name: device.deviceAliasName,
+                        },
+                        native: {},
+                    });
+                    await this.setObjectNotExistsAsync(device.deviceId + ".remote", {
+                        type: "channel",
+                        common: {
+                            name: "Remote Controls",
+                        },
+                        native: {},
+                    });
+                    await this.setObjectNotExistsAsync(device.deviceId + ".general", {
+                        type: "channel",
+                        common: {
+                            name: "General Information",
+                        },
+                        native: {},
+                    });
+
+                    this.json2iob.parse(device.deviceId + ".general", device);
+
+                    this.requestClient({
+                        method: "get",
+                        url: "https://intexiotappservice.azurewebsites.net//api/v1/commandset/device/" + device.deviceId,
+                        headers: {
+                            "Content-Type": "application/json",
+                            Accept: "*/*",
+                            "User-Agent": "Intex/1.0.13 (iPhone; iOS 14.7; Scale/3.00)",
+                            "Accept-Language": "de-DE;q=1, en-DE;q=0.9",
+                            Authorization: "Bearer " + this.session.token,
+                        },
+                    })
+                        .then((res) => {
+                            this.log.debug(JSON.stringify(res.data));
+                            for (const command of res.data) {
+                                if (command.commandName === "Refresh") {
+                                    this.commandObject[device.deviceId] = command.commandData;
+                                }
+                                this.setObjectNotExists(device.deviceId + ".remote." + command.commandName, {
+                                    type: "state",
+                                    common: {
+                                        name: command.commandData,
+                                        type: "boolean",
+                                        role: "boolean",
+                                        write: true,
+                                        read: true,
+                                    },
+                                    native: {},
+                                });
+                            }
+                        })
+                        .catch((error) => {
+                            this.log.error(error);
+                            error.response && this.log.error(JSON.stringify(error.response.data));
+                        });
+                }
+            })
+            .catch((error) => {
+                this.log.error(error);
+                error.response && this.log.error(JSON.stringify(error.response.data));
+            });
+    }
+
+    async updateDevices() {
+        return;
+        this.deviceArray.forEach(async (deviceId) => {
+            await this.requestClient({
+                method: "POST",
+                url: "",
+            })
+                .then((res) => {
+                    this.log.debug(JSON.stringify(res.data));
+                    return res.data;
+                })
+                .catch((error) => {
+                    this.log.error(error);
+                    if (error.response) {
+                        this.log.error(JSON.stringify(error.response.data));
+                    }
+                });
+        });
+    }
+
+    async refreshToken() {
+        await this.requestClient({
+            method: "POST",
+            url: "https://intexiotappservice.azurewebsites.net/api/oauth/auth",
+
+            headers: {
+                accept: "*/*",
+                "content-type": "application/json",
+            },
+            data: { refresh_token: this.session.refresh_token },
+        })
+            .then((res) => {
+                this.log.debug(JSON.stringify(res.data));
+                this.session = res.data;
+                this.setState("info.connection", true, true);
+                return res.data;
+            })
+            .catch((error) => {
+                this.log.error("refresh token failed");
+                this.log.error(error);
+                error.response && this.log.error(JSON.stringify(error.response.data));
+                this.log.error("Start relogin in 1min");
+                this.reLoginTimeout = setTimeout(() => {
+                    this.login();
+                }, 1000 * 60 * 1);
+            });
+    }
+
+    /**
+     * Is called when adapter shuts down - callback has to be called under any circumstances!
+     * @param {() => void} callback
+     */
+    onUnload(callback) {
+        try {
+            this.setState("info.connection", false, true);
+            clearTimeout(this.refreshTimeout);
+            clearTimeout(this.reLoginTimeout);
+            clearTimeout(this.refreshTokenTimeout);
+            clearInterval(this.updateInterval);
+            clearInterval(this.refreshTokenInterval);
+            callback();
+        } catch (e) {
+            callback();
+        }
+    }
+
+    /**
+     * Is called if a subscribed state changes
+     * @param {string} id
+     * @param {ioBroker.State | null | undefined} state
+     */
+    async onStateChange(id, state) {
+        /*
+        	"commandName": "PowerOnOff",
+	"commandData": "8888060F014000"
+    	"commandName": "JetOnOff",
+	"commandData": "8888060F011000"
+
+	"commandName": "BubbleOnOff",
+	"commandData": "8888060F010400"
+
+
+	"commandName": "HeatOnOff",
+	"commandData": "8888060F010010"
+
+	"commandName": "FilterOnOff",
+	"commandData": "8888060F010004"
+
+	"commandName": "SanitizerOnOff",
+	"commandData": "8888060F010001"
+
+	"commandName": "Refresh",
+	"commandData": "8888060FEE0F01"
+
+	"commandName": "TempSet",
+	"commandData": "8888050F0C"
+
+        8888050F0C26A8
+        8888060F01400098
+        8888060FEE0F01DA
+        8888060F010400D4
+        8888060F010004D4
+        8888060F011000C8
+        8888060F010010C8
+        */
+        if (state) {
+            if (!state.ack) {
+                const deviceId = id.split(".")[2];
+                const object = await this.getObjectAsync(id);
+                const objectData = object.common.name;
+                await this.requestClient({
+                    method: "post",
+                    url: "https://intexiotappservice.azurewebsites.net/api/v1/command/" + deviceId,
+                    headers: {
+                        "Content-Type": "application/json",
+                        Accept: "*/*",
+                        "User-Agent": "Intex/1.0.13 (iPhone; iOS 14.7; Scale/3.00)",
+                        "Accept-Language": "de-DE;q=1, en-DE;q=0.9",
+                        Authorization: "Bearer " + this.session.token,
+                    },
+                    data: JSON.stringify({
+                        sid: Date.now() + "000",
+                        type: "1",
+                        data: objectData + this.objectEndings[objectData],
+                    }),
+                })
+                    .then((res) => {
+                        this.log.debug(JSON.stringify(res.data));
+                        return res.data;
+                    })
+                    .catch((error) => {
+                        this.log.error(error);
+                        if (error.response) {
+                            this.log.error(JSON.stringify(error.response.data));
+                        }
+                    });
+                clearTimeout(this.refreshTimeout);
+                this.refreshTimeout = setTimeout(async () => {
+                    await this.updateDevices();
+                }, 10 * 1000);
+            }
+        }
+    }
+}
+
+if (require.main !== module) {
+    // Export the constructor in compact mode
+    /**
+     * @param {Partial<utils.AdapterOptions>} [options={}]
+     */
+    module.exports = (options) => new Intex(options);
+} else {
+    // otherwise start the instance directly
+    new Intex();
+}
